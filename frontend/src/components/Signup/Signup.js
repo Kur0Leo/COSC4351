a9aaae7f57c27cade1f4c1ee617546b9084ebc28
Lena <lenaluc14@gmail.com>
2021-11-24 02:33:39 -0600

    Styled Signup

--- frontend/src/components/Signup/Signup.js
+++ frontend/src/components/Signup/Signup.js
@@ -4,15 +4,22 @@ import { Link } from "react-router-dom";
 
 function Signup(){
     return(
-        <div className="signup">
-             <h1>Reservation System</h1>
-            <h2>Sign Up</h2>
-            <input type="Text" placeholder="First Name"/><br/>
-            <input type="text" placeholder="Last Name"/><br/>
-            <input type="Email" placeholder="Email"/><br/>
-            <input type="Password" placeholder="Password"/><br/>
-            <input type="Password" placeholder="Confirm Password"/><br/>
-            <button>Sign Up</button>
+        <div className="container" >
+            <div className="float-child leftside" ></div>
+            <div className="content">
+                <div className="signup">
+                    <h1>Reservation System</h1>
+                    <h2>Sign Up</h2>
+                    <input style={{marginBottom: "10px"}} type="Text" placeholder="First Name"/><br/>
+                    <input style={{marginBottom: "10px"}} type="text" placeholder="Last Name"/><br/>
+                    <input style={{marginBottom: "10px"}} type="Email" placeholder="Email"/><br/>
+                    <input style={{marginBottom: "10px"}} type="Password" placeholder="Password"/><br/>
+                    <input style={{marginBottom: "10px"}} type="Password" placeholder="Confirm Password"/><br/>
+                    <Link to='/'>
+                    <button className="greenbut">Sign Up</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 
